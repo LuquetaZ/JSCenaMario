4e07b78d57a94f43d88e347ce2bc09e8f4633e06
Lucas Barreto Barbosa <101645521+LucasBarbosa22@users.noreply.github.com>
2022-03-15 12:11:50 -0300

    Update 1
    Update feito em sala de aula no dia 15 de Março

--- teste1/js/test.js
+++ teste1/js/test.js
@@ -2,11 +2,11 @@ var canvas = document.getElementById('exemplo');
 var ctx = canvas.getContext("2d");
 var grdlinear = ctx.createLinearGradient(185, 45, 185, 75);
 
-ctx.fillStyle = '#663300';
+ctx.fillStyle = '#e7631a';
 ctx.beginPath();
 ctx.fillRect(0,340,1000,160);
 ctx.fill();
-
+ 
 ctx.beginPath();
     for(var x = 80 ; x<= 1000; x=x+80){
         ctx.moveTo(x,340);
@@ -16,7 +16,7 @@ ctx.beginPath();
     }
 
 //Parte de cima
-ctx.fillStyle = 'green';
+ctx.fillStyle = '#01ad01';
 ctx.beginPath();
 ctx.fillRect(695,170,200,80);
 ctx.fill();
@@ -28,7 +28,7 @@ ctx.strokeRect(695,170,200,80);
 ctx.stroke();
 
 //Parte de baixo
-ctx.fillStyle = 'green';
+ctx.fillStyle = '#01ad01';
 ctx.beginPath();
 ctx.fillRect(725,240,140,100);
 ctx.fill();
@@ -39,6 +39,24 @@ ctx.beginPath();
 ctx.strokeRect(725,250,140,90);
 ctx.stroke();
 
+ctx.beginPath();
+    for(var x = 84; x<= 1000; x=x+80){
+        ctx.strokeStyle = "#f7d3bd";
+        ctx.moveTo(x,342);
+        ctx.lineTo(x,500);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    }
+
+ctx.beginPath();
+    for(var y = 344; y<= 34400; y=y+50){
+        ctx.strokeStyle = "#f7d3bd";
+        ctx.moveTo(345,y);
+        ctx.lineTo(15,y);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    }
+
 //Linha do Chao - 1
 ctx.strokeStyle = "black";
 ctx.lineWidth = 4;
@@ -48,7 +66,7 @@ ctx.lineTo(1000,340);
 ctx.moveTo(0,420);
 ctx.lineTo(1000,420);
 ctx.stroke();
-
+        
 ctx.strokeStyle = "black";
 ctx.lineWidth = 6;
 ctx.beginPath();
@@ -59,6 +77,7 @@ ctx.fillStyle = '#663300';
 ctx.beginPath();
 ctx.fillRect(130,90,350,70);
 ctx.fill();
+
 
 //ctx.fillStyle='#FFB81C';
 //ctx.beginPath();
